40942ac7618f9a3f5a39d7567a1f0ca5dcf65487
Myeongjun Kim <audwnssp@naver.com>
2024-08-10 02:18:40 +0900

    UserRepository 따라서 만들기

--- src/main/java/com/eastflag/nnc/testkmj/User/UserRepository.java
+++ src/main/java/com/eastflag/nnc/testkmj/User/UserRepository.java
@@ -1,4 +1,10 @@
 package com.eastflag.nnc.testkmj.User;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    // 사용하진 않는데 가능해서 만들어둠
+    // Optional<User> findByTelNum(String email);
 }
