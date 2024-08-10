a1ffb6dc659764848945c1c5d1c9da7247841b64
JNYLee <2071013@hansung.ac.kr>
2024-08-10 18:10:10 +0900

    [refactor]: delete imports

--- src/main/java/com/eastflag/nnc/NncBackendApplication.java
+++ src/main/java/com/eastflag/nnc/NncBackendApplication.java
@@ -1,14 +1,8 @@
 package com.eastflag.nnc;
 
-import com.eastflag.nnc.auth.service.AuthenticationService;
-import com.eastflag.nnc.auth.dto.RegisterRequest;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import static com.eastflag.nnc.user.Role.*;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
