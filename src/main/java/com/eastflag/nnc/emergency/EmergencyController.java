12999f352e5e868e9b1ee35859ba8afd82debb45
JNYLee <2071013@hansung.ac.kr>
2024-08-10 17:36:13 +0900

    [refactor]: White List add, Unusage import delete

--- src/main/java/com/eastflag/nnc/emergency/EmergencyController.java
+++ src/main/java/com/eastflag/nnc/emergency/EmergencyController.java
@@ -3,7 +3,6 @@ package com.eastflag.nnc.emergency;
 import com.eastflag.nnc.common.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 @Log4j2
