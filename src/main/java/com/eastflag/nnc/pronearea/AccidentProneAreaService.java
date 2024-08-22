8c736dc6748bfd48305b0851e2f07ec3662ec6c3
6-keem <6ukeem@gmail.com>
2024-08-22 23:16:25 +0900

    [feat] : 위험지역 API 추가

--- src/main/java/com/eastflag/nnc/pronearea/AccidentProneAreaService.java
+++ src/main/java/com/eastflag/nnc/pronearea/AccidentProneAreaService.java
@@ -21,22 +21,29 @@ public class AccidentProneAreaService {
     }
 
     public CommonResponse findAll(String routeList) {
-        List<AccidentProneArea> accidentProneAreas = accidentProneAreaRepository.findAll();
-        Type coordinatesType = new TypeToken<Coordinates>() {}.getType();
-        Coordinates coordinates = gson.fromJson(routeList, coordinatesType);
-
         ArrayList<AccidentProneArea> result = new ArrayList<>();
-        for(Coordinate coordinate : coordinates.getCoordinates()) {
-            for(AccidentProneArea accidentProneArea : accidentProneAreas) {
-                double distance = DistanceCalculator.calculate(coordinate.getLongitude(),coordinate.getLatitude(),
-                        accidentProneArea.getLongitude(), accidentProneArea.getLatitude()) * 1000;
+        System.out.println("query : " + routeList);
+        try {
+            List<AccidentProneArea> accidentProneAreas = accidentProneAreaRepository.findAll();
+            Type coordinatesType = new TypeToken<Coordinates>() {}.getType();
+            Coordinates coordinates = gson.fromJson(routeList, coordinatesType);
 
-                if(distance < 30 && !result.contains(accidentProneArea)){
-                    result.add(accidentProneArea);
+            System.out.println("coordinates : " + coordinates.toString());
+            for(Coordinate coordinate : coordinates.getCoordinates()) {
+                for(AccidentProneArea accidentProneArea : accidentProneAreas) {
+                    double distance = DistanceCalculator.calculate(coordinate.getLongitude(),coordinate.getLatitude(),
+                            accidentProneArea.getLongitude(), accidentProneArea.getLatitude()) * 1000;
+
+                    if(distance < 30 && !result.contains(accidentProneArea)){
+                        result.add(accidentProneArea);
+                    }
                 }
             }
-        }
 
+            System.out.println("result : " + result);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
         return CommonResponse.builder()
             .code(200)
             .message(ResponseMessage.SUCCESS)
