b536f748ee6946ecac161893e516f38d2e4634b7
6-keem <6ukeem@gmail.com>
2024-08-23 00:32:49 +0900

    도르마무...

--- src/main/java/com/eastflag/nnc/pronearea/AccidentProneAreaService.java
+++ src/main/java/com/eastflag/nnc/pronearea/AccidentProneAreaService.java
@@ -2,28 +2,33 @@ package com.eastflag.nnc.pronearea;
 
 import com.eastflag.nnc.common.CommonResponse;
 import com.eastflag.nnc.common.ResponseMessage;
-import com.eastflag.nnc.pronearea.dto.Coordinate;
-import com.eastflag.nnc.pronearea.dto.Coordinates;
+import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class AccidentProneAreaService {
     private final AccidentProneAreaRepository accidentProneAreaRepository;
+    private final Gson gson = new Gson();
     @Autowired
     public AccidentProneAreaService(AccidentProneAreaRepository accidentProneAreaRepository) {
         this.accidentProneAreaRepository = accidentProneAreaRepository;
     }
 
-    public CommonResponse findAll(Coordinates coordinates) {
+    public CommonResponse findAll(String query) {
         ArrayList<AccidentProneArea> result = new ArrayList<>();
-        System.out.println("coordinates : " + coordinates);
+        System.out.println("query : " + query);
         try {
             List<AccidentProneArea> accidentProneAreas = accidentProneAreaRepository.findAll();
+            Type coordinatesType = new TypeToken<Coordinates>() {}.getType();
+            Coordinates coordinates = gson.fromJson(query, coordinatesType);
+
+            System.out.println("coordinates : " + coordinates.toString());
             for(Coordinate coordinate : coordinates.getCoordinates()) {
                 for(AccidentProneArea accidentProneArea : accidentProneAreas) {
                     double distance = DistanceCalculator.calculate(coordinate.getLongitude(),coordinate.getLatitude(),
@@ -40,9 +45,9 @@ public class AccidentProneAreaService {
             System.out.println(e.getMessage());
         }
         return CommonResponse.builder()
-            .code(200)
-            .message(ResponseMessage.SUCCESS)
-            .data(result)
-            .build();
+                .code(200)
+                .message(ResponseMessage.SUCCESS)
+                .data(result)
+                .build();
     }
 }
