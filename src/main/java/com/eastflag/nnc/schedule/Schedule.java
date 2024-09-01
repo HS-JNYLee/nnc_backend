a837e253197bc54bd5ffb66eab5e56f1d781d318
6-keem <6ukeem@gmail.com>
2024-09-01 15:01:56 +0900

    [refactor] : schedule field 수정

--- src/main/java/com/eastflag/nnc/schedule/Schedule.java
+++ src/main/java/com/eastflag/nnc/schedule/Schedule.java
@@ -32,6 +32,6 @@ public class Schedule {
     private int routeId;
 
     private String address;
-    private Integer isWholeday;
+    private boolean isWholeday;
     private String guideDatetime;
 }
